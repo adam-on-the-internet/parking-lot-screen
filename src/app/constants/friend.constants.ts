0d98c981b492891fe96ddde536afb60ac1a31e8f
Adam Callanan <a.callanan@sbcglobal.net>
2019-08-30 20:01:51 -0500

    added all friends

--- src/app/constants/friend.constants.ts
+++ src/app/constants/friend.constants.ts
@@ -21,4 +21,359 @@ export const FRIEND_IMAGE_LIST: FriendImage[] = [
     name: "Xenomorph",
     tags: ["space"],
   },
+  {
+    src: "friend5.png",
+    name: "Adrien Brody",
+    tags: [],
+  },
+  {
+    src: "friend6.png",
+    name: "Toadette T-Pose",
+    tags: [],
+  },
+  {
+    src: "friend7.png",
+    name: "The Lonely Bridge",
+    tags: [],
+  },
+  {
+    src: "friend8.png",
+    name: "Spooky",
+    tags: [],
+  },
+  {
+    src: "friend9.png",
+    name: "Peach Bike",
+    tags: [],
+  },
+  {
+    src: "friend10.png",
+    name: "Toad Kart",
+    tags: [],
+  },
+  {
+    src: "friend11.gif",
+    name: "Running Mario",
+    tags: [],
+  },
+  {
+    src: "friend12.gif",
+    name: "Running Girl and Cat",
+    tags: [],
+  },
+  {
+    src: "friend13.gif",
+    name: "Nyan Cat",
+    tags: [],
+  },
+  {
+    src: "friend14.gif",
+    name: "Homestar Runner",
+    tags: [],
+  },
+  {
+    src: "friend15.gif",
+    name: "Patriot Parade",
+    tags: [],
+  },
+  {
+    src: "friend16.gif",
+    name: "Running Sonic",
+    tags: [],
+  },
+  {
+    src: "friend17.gif",
+    name: "Running Pikachu",
+    tags: [],
+  },
+  {
+    src: "friend18.gif",
+    name: "Running Skeleton",
+    tags: [],
+  },
+  {
+    src: "friend19.gif",
+    name: "Tiger T-Pose Spinning",
+    tags: [],
+  },
+  {
+    src: "friend20.gif",
+    name: "Spinning Pizza",
+    tags: [],
+  },
+  {
+    src: "friend21.gif",
+    name: "Running Kirby",
+    tags: [],
+  },
+  {
+    src: "friend22.gif",
+    name: "Running Samus",
+    tags: [],
+  },
+  {
+    src: "friend23.gif",
+    name: "Running Yoshi",
+    tags: [],
+  },
+  {
+    src: "friend24.gif",
+    name: "Ghost Chasing Man",
+    tags: [],
+  },
+  {
+    src: "friend25.gif",
+    name: "Running Alien",
+    tags: [],
+  },
+  {
+    src: "friend26.gif",
+    name: "Dancing Alien",
+    tags: [],
+  },
+  {
+    src: "friend27.gif",
+    name: "Spinning 3d UFO",
+    tags: [],
+  },
+  {
+    src: "friend28.gif",
+    name: "Spinning 2d UFO",
+    tags: [],
+  },
+  {
+    src: "friend29.gif",
+    name: "ALF",
+    tags: [],
+  },
+  {
+    src: "friend30.gif",
+    name: "Club Penguin Camera",
+    tags: [],
+  },
+  {
+    src: "friend31.gif",
+    name: "Dancing Toad",
+    tags: [],
+  },
+  {
+    src: "friend32.gif",
+    name: "Dancing Gyroid",
+    tags: [],
+  },
+  {
+    src: "friend33.gif",
+    name: "Dancing Bob Cat",
+    tags: [],
+  },
+  {
+    src: "friend34.gif",
+    name: "Skateboarding Bart",
+    tags: [],
+  },
+  {
+    src: "friend35.gif",
+    name: "Dancing Funky Kong",
+    tags: [],
+  },
+  {
+    src: "friend36.gif",
+    name: "Lakitu Kart",
+    tags: [],
+  },
+  {
+    src: "friend37.gif",
+    name: "Rosalina",
+    tags: [],
+  },
+  {
+    src: "friend38.gif",
+    name: "Dancing Waluigi",
+    tags: [],
+  },
+  {
+    src: "friend39.gif",
+    name: "Spinning Bowser Kart",
+    tags: [],
+  },
+  {
+    src: "friend40.gif",
+    name: "Cheering Toads and Koopas",
+    tags: [],
+  },
+  {
+    src: "friend41.gif",
+    name: "Dancing Daisy",
+    tags: [],
+  },
+  {
+    src: "friend42.gif",
+    name: "Swimming Magi-Karp",
+    tags: [],
+  },
+  {
+    src: "friend43.gif",
+    name: "Weird Squidward",
+    tags: [],
+  },
+  {
+    src: "friend44.gif",
+    name: "Screaming Courage",
+    tags: [],
+  },
+  {
+    src: "friend45.gif",
+    name: "Lazy Skeleton",
+    tags: [],
+  },
+  {
+    src: "friend46.gif",
+    name: "Running Alien Red Eyes",
+    tags: [],
+  },
+  {
+    src: "friend47.gif",
+    name: "Flying Dragon",
+    tags: [],
+  },
+  {
+    src: "friend48.gif",
+    name: "Biking Ness",
+    tags: [],
+  },
+  {
+    src: "friend49.gif",
+    name: "Saxophone Santa",
+    tags: [],
+  },
+  {
+    src: "friend50.gif",
+    name: "Sneaky Skeleton",
+    tags: [],
+  },
+  {
+    src: "friend51.gif",
+    name: "Shaking Pokemon Egg",
+    tags: [],
+  },
+  {
+    src: "friend52.gif",
+    name: "Rolling Yoshi Egg",
+    tags: [],
+  },
+  {
+    src: "friend53.gif",
+    name: "Rolling Jiggly-Puff",
+    tags: [],
+  },
+  {
+    src: "friend54.gif",
+    name: "Strolling Waddle Dee",
+    tags: [],
+  },
+  {
+    src: "friend55.gif",
+    name: "Clippy",
+    tags: [],
+  },
+  {
+    src: "friend56.gif",
+    name: "Strolling Goomba",
+    tags: [],
+  },
+  {
+    src: "friend56.gif",
+    name: "Strolling Goomba",
+    tags: [],
+  },
+  {
+    src: "friend57.gif",
+    name: "Dancing Cat",
+    tags: [],
+  },
+  {
+    src: "friend58.gif",
+    name: "Trombone Cat",
+    tags: [],
+  },
+  {
+    src: "friend59.gif",
+    name: "Laughing Skeletor",
+    tags: [],
+  },
+  {
+    src: "friend60.gif",
+    name: "Running Game and Watch",
+    tags: [],
+  },
+  {
+    src: "friend61.gif",
+    name: "Super Mario 64 Logo",
+    tags: [],
+  },
+  {
+    src: "friend62.gif",
+    name: "Idle Link",
+    tags: [],
+  },
+  {
+    src: "friend63.gif",
+    name: "Floating Metroid",
+    tags: [],
+  },
+  {
+    src: "friend64.gif",
+    name: "Spinning Floof",
+    tags: [],
+  },
+  {
+    src: "friend65.gif",
+    name: "Sipping Kermit",
+    tags: [],
+  },
+  {
+    src: "friend66.gif",
+    name: "Yellow Submarine",
+    tags: [],
+  },
+  {
+    src: "friend67.gif",
+    name: "Walking Zombie",
+    tags: [],
+  },
+  {
+    src: "friend68.gif",
+    name: "Swimming Corgi",
+    tags: [],
+  },
+  {
+    src: "friend69.gif",
+    name: "Sitting Elephant",
+    tags: [],
+  },
+  {
+    src: "friend70.gif",
+    name: "Running Egg-Man",
+    tags: [],
+  },
+  {
+    src: "friend71.gif",
+    name: "Gavel Hit",
+    tags: [],
+  },
+  {
+    src: "friend72.gif",
+    name: "Phoenix Wright Pointing",
+    tags: [],
+  },
+  {
+    src: "friend73.gif",
+    name: "Shy Attorney",
+    tags: [],
+  },
+  {
+    src: "friend74.gif",
+    name: "Shrugging Miles Edgeworth",
+    tags: [],
+  },
 ];
