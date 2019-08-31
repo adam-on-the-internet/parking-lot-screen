7008fbb8a7444b7db5ab90a53e7131de61f01c6b
Adam Callanan <a.callanan@sbcglobal.net>
2019-08-31 13:29:09 -0500

    built out friend list

--- src/app/components/gallery/friend-gallery/friend-gallery.component.ts
+++ src/app/components/gallery/friend-gallery/friend-gallery.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { DetailedImage } from 'src/app/models/Image.model';
+import { FRIEND_IMAGE_LIST } from 'src/app/constants/friend.constants';
 
 @Component({
   selector: 'app-friend-gallery',
   templateUrl: './friend-gallery.component.html',
   styleUrls: ['./friend-gallery.component.css']
 })
-export class FriendGalleryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class FriendGalleryComponent {
+  public friendImageList: DetailedImage[] = FRIEND_IMAGE_LIST;
 }
