9ae06c8d7936dd3549d573bab1ab2ccdf1755bd3
Adam Callanan <a.callanan@sbcglobal.net>
2019-08-31 13:58:40 -0500

    built out functional gallery switching

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent implements OnInit {
-  constructor() { }
+export class GalleryComponent {
+  public mode = "";
 
-  ngOnInit() {
+  public get modeNotSet(): boolean {
+    return !this.friendMode && !this.locationMode;
+  }
+
+  public get friendMode(): boolean {
+    return this.mode === "FRIEND";
+  }
+
+  public get locationMode(): boolean {
+    return this.mode === "LOCATION";
+  }
+
+  public setMode(mode: string): void {
+    this.mode = mode;
   }
 
 }
