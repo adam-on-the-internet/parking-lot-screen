ff29e07b9a1ceb5abd87ac7833f1e52273f6e78c
Adam Callanan <a.callanan@sbcglobal.net>
2019-09-05 18:33:25 -0500

    added song constants

---
+++ src/app/constants/song.constants.ts
@@ -0,0 +1,47 @@
+import { Song } from '../models/Song.model';
+
+export const DISCLAIMER: Song =     {
+    name: "alien pt. 0: disclaimer",
+    tags: ["legal"],
+};
+
+export const EYES: Song =     {
+    name: "alien pt. 1: in your eyes",
+    tags: ["alien"],
+};
+export const LOVER: Song =     {
+    name: "alien pt. 2: alien lover",
+    tags: ["alien"],
+};
+export const FEELS: Song =     {
+    name: "alien pt. 3: now i know what it feels like to be an alien",
+    tags: ["alien"],
+};
+export const SHIRT: Song =     {
+    name: "do i like you or do i like your shirt",
+    tags: [],
+};
+export const AUNT: Song =     {
+    name: "not your aunt",
+    tags: [],
+};
+export const VAGUE: Song =     {
+    name: "vaguely into you",
+    tags: [],
+};
+export const HAUNT: Song = {
+    name: "somebody to haunt",
+    tags: ["spooky"],
+};
+export const DOG: Song = {
+    name: "about your dog",
+    tags: [],
+};
+export const JOHNNY: Song = {
+    name: "oh johnny",
+    tags: [],
+};
+export const THEME: Song = {
+    name: "now you're in the parking lot",
+    tags: [],
+};
