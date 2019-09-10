33c25cecd226f603b644e4dd5968f4332f5677b9
Adam Callanan <a.callanan@sbcglobal.net>
2019-09-09 21:51:50 -0500

    added ghost of liberace song TO sep playlist

--- src/app/constants/playlist.constants.ts
+++ src/app/constants/playlist.constants.ts
@@ -1,4 +1,4 @@
-import { LOVER, HAUNT, DISCLAIMER, AUNT, VAGUE, DOG, EYES, FEELS, CARDIFF, ALL_SONGS } from './song.constants';
+import { LOVER, HAUNT, DISCLAIMER, AUNT, VAGUE, DOG, EYES, FEELS, CARDIFF, ALL_SONGS, LIBERACE } from './song.constants';
 import { Playlist } from '../models/Playlist.model';
 
 const TEST_PLAYLIST: Playlist = {
@@ -26,6 +26,7 @@ const AQUADOME_SEP_13_19: Playlist = {
         AUNT,
         VAGUE,
         HAUNT,
+        LIBERACE,
         DOG,
         DISCLAIMER,
         EYES,
