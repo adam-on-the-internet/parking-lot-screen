81e972886b2dc546ec717c7222978bd1be468667
Adam Callanan <a.callanan@sbcglobal.net>
2019-09-23 20:42:09 -0500

    add reverse animations

--- src/app/services/universe.service.ts
+++ src/app/services/universe.service.ts
@@ -7,5 +7,5 @@ export class UniverseService {
   public friendsPerScene = 6;
   public secondsPerScene = 40;
   public availableSpeeds = 16;
-  public availableAnimations = 6;
+  public availableAnimations = 12;
 }
