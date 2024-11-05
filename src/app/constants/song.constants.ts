0690b001d53469f455956933e4fc7e754285116a
Adam Callanan <a.callanan@sbcglobal.net>
2024-11-05 00:25:17 -0600

    add avail playlist, format

--- src/app/constants/song.constants.ts
+++ src/app/constants/song.constants.ts
@@ -1,96 +1,96 @@
-import { Song } from '../models/Song.model';
+import {Song} from '../models/Song.model';
 
-export const DISCLAIMER: Song =     {
-    name: "alien pt. 0: disclaimer",
-    tags: ["legal"],
+export const DISCLAIMER: Song = {
+  name: "alien pt. 0: disclaimer",
+  tags: ["legal"],
 };
 
-export const EYES: Song =     {
-    name: "alien pt. 1: in your eyes",
-    tags: ["love", "eyes"],
+export const EYES: Song = {
+  name: "alien pt. 1: in your eyes",
+  tags: ["love", "eyes"],
 };
 
-export const LOVER: Song =     {
-    name: "alien pt. 2: alien lover",
-    tags: ["alien"],
+export const LOVER: Song = {
+  name: "alien pt. 2: alien lover",
+  tags: ["alien"],
 };
 
-export const FEELS: Song =     {
-    name: "alien pt. 3: now i know what it feels like to be an alien",
-    tags: ["alien"],
+export const FEELS: Song = {
+  name: "alien pt. 3: now i know what it feels like to be an alien",
+  tags: ["alien"],
 };
 
-export const SHIRT: Song =     {
-    name: "do i like you or do i like your shirt",
-    tags: ["shirt", "love"],
+export const SHIRT: Song = {
+  name: "do i like you or do i like your shirt",
+  tags: ["shirt", "love"],
 };
 
-export const AUNT: Song =     {
-    name: "not your aunt",
-    tags: ["aunt"],
+export const AUNT: Song = {
+  name: "not your aunt",
+  tags: ["aunt"],
 };
 
-export const VAGUE: Song =     {
-    name: "vaguely into you",
-    tags: [],
+export const VAGUE: Song = {
+  name: "vaguely into you",
+  tags: [],
 };
 
-export const MEDIOCRE: Song =     {
-    name: "mediocre sex",
-    tags: [],
+export const MEDIOCRE: Song = {
+  name: "mediocre sex",
+  tags: [],
 };
 
-export const HATE: Song =     {
-    name: "i hate your husband",
-    tags: [],
+export const HATE: Song = {
+  name: "i hate your husband",
+  tags: [],
 };
 
-export const EVERYWHERE: Song =     {
-    name: "everywhere you are (theme song reprise)",
-    tags: [],
+export const EVERYWHERE: Song = {
+  name: "everywhere you are (theme song reprise)",
+  tags: [],
 };
 
 export const HAUNT: Song = {
-    name: "somebody to haunt",
-    tags: ["spooky"],
+  name: "somebody to haunt",
+  tags: ["spooky"],
 };
 
 export const SUMMER: Song = {
-    name: "what you did last summer",
-    tags: ["spooky"],
+  name: "what you did last summer",
+  tags: ["spooky"],
 };
 
 export const DOG: Song = {
-    name: "about your dog",
-    tags: ["dog"],
+  name: "about your dog",
+  tags: ["dog"],
 };
 
 export const JOHNNY: Song = {
-    name: "oh johnny",
-    tags: ["snow"],
+  name: "oh johnny",
+  tags: ["snow"],
 };
 
 export const THEME: Song = {
-    name: "adam in the parking lot (theme song)",
-    tags: [],
+  name: "adam in the parking lot (theme song)",
+  tags: [],
 };
 
 export const CARDIFF: Song = {
-    name: "pasta in cardiff",
-    tags: ["cooking", "cardiff"],
+  name: "pasta in cardiff",
+  tags: ["cooking", "cardiff"],
 };
 
 export const NEVER: Song = {
-    name: "i never know what to say",
-    tags: [],
+  name: "i never know what to say",
+  tags: [],
 };
 
 export const THANKS: Song = {
-    name: "thanks for coming, tip your bartenders",
-    tags: [],
+  name: "thanks for coming, tip your bartenders",
+  tags: [],
 };
 
 export const BERNIE: Song = {
-    name: "thanks for coming, vote Bernie",
-    tags: [],
+  name: "thanks for coming, vote Bernie",
+  tags: [],
 };
